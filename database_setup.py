c427fac535dab536fee15a649d03a0b7f4440532
Michaelbretagne <michael.bretagne@gmail.com>
2016-11-02 22:46:27 -0700

    Added JSON feature

--- database_setup.py
+++ database_setup.py
@@ -20,8 +20,6 @@ class Country(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    # user_id = Column(Integer, ForeignKey('user.id'))
-    # user = relationship(User)
 
     @property
     def serialize(self):
@@ -38,8 +36,6 @@ class Region(Base):
     name = Column(String(80), nullable=False)
     country_id = Column(Integer, ForeignKey('country.id'))
     country = relationship(Country)
-    # user_id = Column(Integer, ForeignKey('user.id'))
-    # user = relationship(User)
 
     @property
     def serialize(self):
@@ -54,6 +50,7 @@ class Brewery(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(80), nullable=False)
+    image = Column(String(80), default="static/img/uploaded_images/not_available.jpg")
     country_id = Column(Integer, ForeignKey('country.id'))
     region_id = Column(Integer, ForeignKey('region.id'))
     user_id = Column(Integer, ForeignKey('user.id'))
@@ -73,12 +70,12 @@ class Beer(Base):
     __tablename__ = 'beer'
 
     id = Column(Integer, primary_key=True)
-    image = Column(String(80), default="static/uploads/not_available.jpg")
+    image = Column(String(80), default="static/img/uploaded_images/not_available.jpg")
     name = Column(String(80), nullable=False)
     style = Column(String(80)) # eg. IPA, PALE ALE...
     abv = Column(Integer) # Alcohol By Volume
     ibu = Column(Integer) # International Bitterness Units
-    description = Column(String(400)) # Description of the beer
+    description = Column(String(700)) # Description of the beer
     country_id = Column(Integer, ForeignKey('country.id'))
     region_id = Column(Integer, ForeignKey('region.id'))
     brewery_id = Column(Integer, ForeignKey('brewery.id'))
@@ -94,6 +91,10 @@ class Beer(Base):
         return {
             'name': self.name,
             'id': self.id,
+            'style': self.style,
+            'abv': self.abv,
+            'ibu': self.ibu,
+            'description': self.description,
         }
 
 class Rating(Base):
